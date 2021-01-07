0ef32d0ef65e118ea5818af7ac705df9397b08f9
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-07 09:16:33 -0500

    Set up Postgres db

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 1725;
+const port = process.env.PORT || 1725;
 
+// sync server to db
+const db = require("./models");
+db.sequelize.sync();
+
+// test route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// listen for requests
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
