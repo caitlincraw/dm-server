6ec192644f6e8c7bbfb28797977380206154bdb2
Rudy Saavedra <rudysa2@gmail.com>
2021-01-07 21:58:10 -0500

    added socket.io for server

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 const port = process.env.PORT || 1725;
 
 // parse requests of content-type - application/json
@@ -19,4 +27,15 @@ app.get('/', (req, res) => {
 // listen for requests
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+io.on('connection', (socket) => {
+  let addedUser = false;
+
+  socket.on('chat', (data) => {
+      socket.broadcast.emit('chat', {
+          // username: socket.username,
+          message: data
+      });
+  });
 });
