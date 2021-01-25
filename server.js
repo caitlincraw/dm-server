c907a08956b69de42783353286bf785e59a08150
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-24 21:04:46 -0500

    clean up server.js

--- server.js
+++ server.js
@@ -41,13 +41,7 @@ const sessionMiddleware = session({
   }
 });
 
-app.set('trust proxy', 2); // trust first proxy
-
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.set('trust proxy', 2); // trust first proxy
-//   sessionConfig.cookie.secure = true; // serve secure cookies
-// }
+app.set('trust proxy', 2); 
 
 app.use(sessionMiddleware);
 
